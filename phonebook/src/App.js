f5f068d77c4448af7039bb4b4d252eb1a503a43c
Nguyen Duc <mduc0123@gmail.com>
2022-12-12 14:27:06 +0200

    Finish part 2

--- phonebook/src/App.js
+++ phonebook/src/App.js
@@ -3,10 +3,12 @@ import { personService } from './services/persons'
 import { Filter } from './Filter'
 import { Persons } from './Persons'
 import { PersonForm } from './PersonForm'
+import { Notification } from './Notification'
 
 
 const App = () => {
   const [persons, setPersons] = useState([]); 
+  const [notification_data, setNotificationData] = useState({messages: null, type: null}); 
 
   useEffect(() => {
     getListPersons();
@@ -26,6 +28,7 @@ const App = () => {
       const status = window.confirm(`${person.name} is already added to phonebook, replace the old number with a new one?`);
       if (status) {
         personService.update(find.id, person).then(() => {
+          pushNotification(`Changed ${person.name}`);
           getListPersons();
         });
       }
@@ -34,7 +37,7 @@ const App = () => {
         const persons_clone = [...persons];
         persons_clone.push(response.data);
         setPersons(persons_clone);
-        alert(`${response.data.name} is already added to phonebook`)
+        pushNotification(`Added ${response.data.name}`);
       });
     }
   }
@@ -42,12 +45,28 @@ const App = () => {
   const handleRemovePerson = (person) => {
     personService.delete(person.id).then(() => {
       getListPersons();
+    }).catch(() => {
+      pushNotification(`Information of ${person.name} has already been removed form server`, 'e')
+    })
+  }
+
+  const pushNotification = (messages, type) => {
+    setNotificationData({
+      messages: messages,
+      type: type
     });
+    setTimeout(() => {
+      setNotificationData({
+        messages: null,
+        type: null
+      })
+    }, 5000);
   }
 
   return (
     <div>
       <h2>Phonebook</h2>
+      <Notification messages={notification_data.messages} type={notification_data.type} />
       <Filter persons={persons} />
       <h3>Add a new</h3>
       <PersonForm handleAddNewPerson={(p) => {handleAddNewPerson(p)}} persons={persons} />
